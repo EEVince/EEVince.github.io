4c286ec22ab108660d13424b0e8029ccb4d6bc15
EEVince <t.vince006@gmail.com>
2024-08-20 15:06:44 +0200

    This has got to work!

--- js/blog-loader.js
+++ js/blog-loader.js
@@ -8,11 +8,10 @@ function generatePostEmbed()
 {
 	emb = document.createElement("embed");
 	const srcData = sessionStorage.getItem("post-src");
-	stylePost(srcData); //change style tag!
-	emb.setAttribute("src", srcData);
-	console.log(srcData + "\t attr:" + emb.getAttribute("src"));
+	var postTxt = stylePost(srcData);//change style!
+	console.log(postTxt); 
 	mainDiv = document.getElementsByClassName("main-content")[0];
-	mainDiv.appendChild(emb); //add embed tag and so, DOM.
+	mainDiv.innerHTML = postTxt.body;
 }
 function stylePost(srcData)
 {
@@ -26,6 +25,7 @@ function stylePost(srcData)
 		const mainTxt = doc.getElementById("txt-body");
 		mainTxt.setAttribute("style", styleInject);
 		console.log(mainTxt.getAttribute("style"));
+		return doc.documentElement.innerHTML; //convert back to html
 	});
 }
 loadIntoEmbed();
