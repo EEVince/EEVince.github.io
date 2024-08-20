943162539553a1b83d76ffd114e46c22e89d2730
EEVince <t.vince006@gmail.com>
2024-08-20 15:30:13 +0200

    Converted stylePost to synchronous

--- js/blog-loader.js
+++ js/blog-loader.js
@@ -8,24 +8,22 @@ function generatePostEmbed()
 {
 	emb = document.createElement("embed");
 	const srcData = sessionStorage.getItem("post-src");
-	const postTxt = stylePost(srcData);//change style!
+	const postTxt = await stylePost(srcData);//change style!
 	console.log(postTxt); 
 	mainDiv = document.getElementsByClassName("main-content")[0];
 	mainDiv.innerHTML = postTxt;
 }
-function stylePost(srcData)
-{
-	fetch(srcData)
-    .then(response => response.text())
-    .then(html => {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(html, 'text/html');
-		const styleInject = "clear:both;"; // doc style code
-		const mainTxt = doc.getElementById("txt-body");
-		mainTxt.setAttribute("style", styleInject);
-		console.log(mainTxt.getAttribute("style"));
-		return doc.body.innerHTML;
-	});
+async function stylePost(srcData) {
+    const response = await fetch(srcData);
+    const html = await response.text();
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+    
+    const styleInject = "clear:both;"; // doc style code
+    const mainTxt = doc.getElementById("txt-body");
+    mainTxt.setAttribute("style", styleInject);
+    console.log(mainTxt.getAttribute("style"));
+    
+    return doc.body.innerHTML;
 }
 loadIntoEmbed();
 generatePostEmbed();
